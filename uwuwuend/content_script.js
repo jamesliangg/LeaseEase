3a37b001f0168d4d7faf4b08f456817221abbe0c
James Liang <86000097+jamesliangg@users.noreply.github.com>
2024-01-27 20:45:28 -0500

    Now edits paragraphs containing hrefs

--- uwuwuend/content_script.js
+++ uwuwuend/content_script.js
@@ -4,11 +4,34 @@ async function replaceText() {
     const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td ,caption, span, a');
     // const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td ,caption');
     for (let i = 0; i < text.length; i++) {
-        if (text[i].innerHTML.includes("href") || text[i].innerHTML.includes("http")) {
+        if (text[i].innerHTML.includes("img")) {
             continue;
         }
+        else if (text[i].innerHTML.includes("href")) {
+            let children = text[i].childNodes;
+            for (let j = 0; j < children.length; j++) {
+                let node_value = children[j].nodeValue;
+                if (node_value != null && !node_value.includes('http')) {
+                    // console.log(node_value);
+                    let url = new URL("http://localhost:3000/");
+                    url.searchParams.append("query", node_value);
+                    try {
+                        let response = await fetch(url);
+                        let data = await response.json();
+                        console.log(node_value);
+                        console.log(data.result);
+                        text[i].childNodes[j].nodeValue = data.result;
+                    } catch (error) {
+                        console.error(error);
+                    }
+                }
+            }
+            continue;
+        }
+        // else if (text[i].innerHTML.includes("http")) {
+        //     continue;
+        // }
         let element = text[i].innerText;
-        // console.log(element)
         let url = new URL("http://localhost:3000/");
         url.searchParams.append("query", element);
         try {

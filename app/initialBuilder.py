e45d399a890f608530322402d056b8e3121420d2
Shreyas Rao <raoshreyas2004@gmail.com>
2024-01-28 04:35:41 -0500

    Fixed the bugs in the pdf automation file

--- app/initialBuilder.py
+++ app/initialBuilder.py
@@ -14,7 +14,7 @@ def T1_Form(lf_name: str, ll_name: str, l_street: str, l_unit: str,
 
   uploadedFileUrl = "https://tribunalsontario.ca/documents/ltb/Tenant%20Applications%20&%20Instructions/T1.pdf"
 
-  fieldsStrings = f"1;form1[0].#subform[1].Rental_Address_for_Landlord_form[0].StreetName[0]{l_street}|1;form1[0].#subform[1].Rental_Address_for_Landlord_form[0].Municipality[0]{l_municipality}|1;form1[0].#subform[1].Rental_Address_for_Landlord_form[0].PostalCode[0]{l_post_code}|1;form1[0].#subform[1].LTHAS_Tenant_Name__Address[0].TFirstName1[0]{tf_name}|1;form1[0].#subform[1].LTHAS_Tenant_Name__Address[0].TLastName1[0]{tl_name}|1;form1[0].#subform[1].LTHAS_Tenant_Name__Address[0].TDayPhone[0]{td_number}|1;form1[0].#subform[1].LTHAS_Tenant_Name__Address[0].Temail[0]{te_mail}|2;form1[0].#subform[5].LTHAS_Landlord_Name__Address[0].LFirstName[0]{lf_name}|2;form1[0].#subform[5].LTHAS_Landlord_Name__Address[0].LLastName[0]{ll_name}|2;form1[0].#subform[5].LTHAS_Landlord_Name__Address[0].LAddress[0]{l_street}|2;form1[0].#subform[5].LTHAS_Landlord_Name__Address[0].LPostalCode[0]{l_post_code}|2;form1[0].#subform[5].LTHAS_Landlord_Name__Address[0].LMunicipality[0]{l_municipality}|2;form1[0].#subform[5].LTHAS_Landlord_Name__Address[0].Lemail[0];{le_email}"
+  fieldsStrings = f"1;form1[0].#subform[1].Rental_Address_for_Landlord_form[0].StreetName[0];{t_street}|1;form1[0].#subform[1].Rental_Address_for_Landlord_form[0].Municipality[0];{t_municipality}|1;form1[0].#subform[1].Rental_Address_for_Landlord_form[0].PostalCode[0];{t_post_code}|1;form1[0].#subform[1].LTHAS_Tenant_Name__Address[0].TFirstName1[0];{tf_name}|1;form1[0].#subform[1].LTHAS_Tenant_Name__Address[0].TLastName1[0];{tl_name}|1;form1[0].#subform[1].LTHAS_Tenant_Name__Address[0].TDayPhone[0];{td_number}|1;form1[0].#subform[1].LTHAS_Tenant_Name__Address[0].Temail[0];{te_mail}|2;form1[0].#subform[5].LTHAS_Landlord_Name__Address[0].LFirstName[0];{lf_name}|2;form1[0].#subform[5].LTHAS_Landlord_Name__Address[0].LLastName[0];{ll_name}|2;form1[0].#subform[5].LTHAS_Landlord_Name__Address[0].LAddress[0];{l_street}|2;form1[0].#subform[5].LTHAS_Landlord_Name__Address[0].LPostalCode[0];{l_post_code}|2;form1[0].#subform[5].LTHAS_Landlord_Name__Address[0].LMunicipality[0];{l_municipality}|2;form1[0].#subform[5].LTHAS_Landlord_Name__Address[0].Lemail[0];{le_email}"
 
   destFile = f"./forms/{outputFileName}"
 
@@ -64,7 +64,7 @@ def N7_Form(lf_name: str, ll_name: str, l_street: str, l_unit: str,
   uploadedFileUrl = "https://tribunalsontario.ca/documents/ltb/Notices%20of%20Termination%20&%20Instructions/N7.pdf"
   t_name = tf_name + " " + tl_name
   l_name = lf_name + " " + ll_name
-  fieldsStrings = f"0;form1[0].#subform[0].Notice_Name_and_Address[0].TO_TenameName[0]{t_name}|0;form1[0].#subform[0].Notice_Name_and_Address[0].From_LandlordName[0];{l_name}|0;form1[0].#subform[0].Notice_Name_and_Address[0].RentalUnitAddress[0];{t_street}|1;form1[0].#subform[4].Signature_for_Notice[0].Signature[0];{l_name}"
+  fieldsStrings = f"0;form1[0].#subform[0].Notice_Name_and_Address[0].TO_TenameName[0];{t_name}|0;form1[0].#subform[0].Notice_Name_and_Address[0].From_LandlordName[0];{l_name}|0;form1[0].#subform[0].Notice_Name_and_Address[0].RentalUnitAddress[0];{t_street}|1;form1[0].#subform[4].Signature_for_Notice[0].Signature[0];{l_name}|1;form1[0].#subform[4].Signature_for_Notice[0].RFirstName[0];{lf_name}|1;form1[0].#subform[4].Signature_for_Notice[0].RLastName[0];{ll_name}"
 
   destFile = f"./forms/{outputFileName}"
 
